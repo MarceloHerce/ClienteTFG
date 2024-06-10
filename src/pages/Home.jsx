e2cf38d5ca6c3c8a4c69cf9afa4c400186d9b11b
MarceloHerce <120047392+MarceloHerce@users.noreply.github.com>
2024-06-10 11:47:14 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,8 +23,6 @@ function Home() {
     return (
         <>
             <HomeText openModal={openModal} />
-            <p>Se actualiza</p>
-            <p>actualizacion</p>
             {isModalOpen && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
                 <div className="bg-black p-6 rounded shadow-lg">
